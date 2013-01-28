dea84c81ab59fba8a6af0359d93b1d364e8cd7e4
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-01-28 16:02:46 +0100

    Fixed the payment logic. Added the backend order interface.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,16 @@
 isotope_transferujpl Changelog
 ==============================
 
+Version 1.1.0 stable (2013-01-28)
+---------------------------------
+
+### Improved
+- Added the backend order interface
+
+### Fixed
+- Fixed the payment logic
+
+
 Version 1.0.0 stable (2013-01-26)
 ---------------------------------
 
--- languages/en/tl_iso_payment_modules.php
+++ languages/en/tl_iso_payment_modules.php
@@ -16,4 +16,4 @@
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_iso_payment_modules']['transferujpl_id']   = array('Transferuj.pl ID', 'Please enter your unique Transferuj.pl ID.');
-$GLOBALS['TL_LANG']['tl_iso_payment_modules']['transferujpl_code'] = array('Seller code', 'Here you can enter the seller code.');
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['transferujpl_code'] = array('Security code', 'Here you can enter the security code you set in the Transferuj.pl panel.');
