798ecc40a9cb5f2897e39dc200656e8585d877a6
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-09-16 20:01:12 +0200

    Updated the readme file. Updated the copyright information

--- PaymentTransferujPl.php
+++ PaymentTransferujPl.php
@@ -2,11 +2,11 @@
 
 /**
  * isotope_transferujpl extension for Contao Open Source CMS
- * 
- * Copyright (C) 2012 Codefog
- * 
+ *
+ * Copyright (C) 2013 Codefog Ltd
+ *
  * @package isotope_transferujpl
- * @link    http://codefog.pl
+ * @author  Codefog Ltd <http://codefog.pl>
  * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
  * @license LGPL
  */
@@ -14,7 +14,7 @@
 
 /**
  * Class PaymentTransferujPl
- * 
+ *
  * Provide a payment method "Transferuj.pl" for Isotope.
  */
 class PaymentTransferujPl extends IsotopePayment
